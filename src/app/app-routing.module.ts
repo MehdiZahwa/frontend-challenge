d0ac94f8cf9718e9048107d81eb432a2c1cf92ef
MehdiZahwa <zahouani.mehdi06@gmail.com>
2020-08-09 01:46:20 +0100

    project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: 'repos',
+        loadChildren: () =>
+          import('./modules/repos/repos.module').then((m) => m.ReposModule),
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NoPageFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoPageFoundComponent
   ],
   imports: [
     BrowserModule,
